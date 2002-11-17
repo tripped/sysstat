3b0cd3a7d50a4451612c1177ee0b401ee2501943
Robert Luberda <robert@debian.org>
2002-11-17 21:42:21 +0100

    New upstream version.
    * New upstream version.
    * Don't run /etc/init.d/sysstat on package install/upgrade (use
      `dh_installinit --no-start' for this).
    * debian/rules, debian/fix.sh: minor changes in the build process.

--- debian/fix.sh
+++ debian/fix.sh
@@ -1,32 +1,42 @@
 #!/bin/sh
+# $Id: fix.sh,v 1.6 2002-11-17 20:42:21 robert Exp $
 
 set -e 
 
-if [ "x$1" = "x" ] ; then
-	echo "Usage: $0 directory"
+if [ "X$1" = "Xsysstat" ] ; then
+	FFILES="usr/lib/sysstat/sa[12]
+		usr/share/man/man*/*
+		usr/share/doc/sysstat/FAQ
+		usr/share/doc/sysstat/examples/*"
+elif [ "X$1" = "Xisag" ] ; then
+	FFILES="usr/share/man/man*/*" 
+else
+	echo "Usage: $0 [ sysstat | isag ]" 1>&2
 	exit 1
 fi
 
-cd "$1"
-mkdir -p -m 755 usr/lib/sysstat
-mv usr/bin/sar usr/bin/sar.sysstat
-mv usr/share/man/man1/sar.1 usr/share/man/man1/sar.sysstat.1
-mv usr/lib/sa/* usr/lib/sysstat
-rmdir usr/lib/sa
-rm -rf usr/doc
+dir="debian/$1"
 
-for file in usr/lib/sysstat/sa[12] usr/share/man/man*/*; do
+if [ ! -d "$dir" ] ; then
+	echo "Directory $dir does not exists!" 1>&2
+	exit 1
+fi
+
+cd "$dir"
+
+for file in `echo $FFILES`; do
  if grep -q 'l[oi][gb]/sa' "$file" >/dev/null 2>&1 ; then
+	echo  " + processing file: $dir/$file"
 	mv "$file" _tmp_
-	sed -e 's|usr/lib/sa|usr/lib/sysstat|' \
-	    -e 's|var/log/sa|var/log/sysstat|' \
+	sed -e 's|usr/lib/sa|usr/lib/sysstat|g' \
+	    -e 's|var/log/sa|var/log/sysstat|g' \
+	    -e 's|usr/local/lib/sa|usr/local/lib/sysstat|g' \
 		< _tmp_ > "$file"
+	chmod --reference=_tmp_ "$file" 
 	touch -r _tmp_	"$file"
 	rm -f _tmp_
  fi
 done
-
-chmod 755 usr/lib/sysstat/sa[12]
 
 exit 0
 

9d39332e69e24724ffc0c01d9bf83d81736c7913
Robert Luberda <robert@debian.org>
2005-05-18 20:43:06 +0200

    New upstream version (stable).

--- debian/sysstat.cron.d
+++ debian/sysstat.cron.d
@@ -9,4 +9,4 @@ ENABLED=false
 5-55/10 * * * * root [ -x /usr/lib/sysstat/sa1 ] && { [ -r "$DEFAULT" ] && . "$DEFAULT" ; [ "$ENABLED" = "true" ] && exec /usr/lib/sysstat/sa1; }
 
 # Daily summary prepared at 19:05.
-5 19 * * * root [ -x /usr/lib/sysstat/sa2 ] && { [ -r "$DEFAULT" ] && . "$DEFAULT" ; [ "$ENABLED" = "true" ]  && exec /usr/lib/sysstat/sa2; }
+5 19 * * * root [ -x /usr/lib/sysstat/sa2 ] && { [ -r "$DEFAULT" ] && . "$DEFAULT" ; [ "$ENABLED" = "true" ]  && exec /usr/lib/sysstat/sa2 -A; }

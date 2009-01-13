316e45418d6782c2547638c0675f2f67aa4133da
Robert Luberda <robert@debian.org>
2009-01-13 01:01:37 +0100

    Add debian-sa1 script and ran it from cron.d job (closes: #499461).

--- debian/sysstat.cron.d
+++ debian/sysstat.cron.d
@@ -1,13 +1,9 @@
-# Global variables:
-#
-#  our configuration file
-DEFAULT=/etc/default/sysstat
-#  default setting, overriden in the above file
-ENABLED=false
-SA1_OPTIONS=""
+# The first element of the path is a directory where the debian-sa1
+# script is located
+PATH=/usr/lib/sysstat:/usr/sbin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # Activity reports every 10 minutes everyday
-5-55/10 * * * * root [ -x /usr/lib/sysstat/sa1 ] && { [ -r "$DEFAULT" ] && . "$DEFAULT" ; [ "$ENABLED" = "true" ] && exec /usr/lib/sysstat/sa1 $SA1_OPTIONS 1 1 ; }
+5-55/10 * * * * root command -v debian-sa1 > /dev/null && debian-sa1 1 1
 
 # Additional run at 23:59 to rotate the statistics file
-59 23 * * * root [ -x /usr/lib/sysstat/sa1 ] && { [ -r "$DEFAULT" ] && . "$DEFAULT" ; [ "$ENABLED" = "true" ] && exec /usr/lib/sysstat/sa1 $SA1_OPTIONS 60 2 ; }
+59 23 * * * root command -v debian-sa1 > /dev/null && debian-sa1 60 2
